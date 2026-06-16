01dd266f264449593aadf8496423cb9d3c1a5787
Samus1906s <sacerdasce@est.utn.ac.cr>
2026-06-16 11:23:11 -0600

    Versión 4, trabajamos en hacer que se de la lectura a JSON y ahoara retorna resulatdos

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,6 +9,8 @@ const VERSION = process.env.SERVER_VERSION;
 const DESCRIPTION = process.env.SERVER_DESCRIPTION;
 const PORT = process.env.SERVER_PORT || 4000;
 
+
+
 const app = express();
 
 app.use(cors());
@@ -22,6 +24,49 @@ app.get( '/', (reg, res) => {
         description: DESCRIPTION,
         puerto: PORT
     })
+});
+
+app.post('/api/parqueo/calcular', (req, res ) => {
+const{placa, tipo, horas, minutos} = req.body;
+
+if(!placa || placa.trim()==""){
+    return res.status(400).json({error: "La placa es requerida"})
+}
+
+if(!tipo || (!tipo=="carro" && tipo=="moto")){
+    return res.status(400).json({error: "El tipo es requerido"})
+}
+
+
+
+if (Number.isNaN(horas) || horas < 0){
+   return res.status(400).json({error: "La cantidad de horas ingresadas"}) 
+}
+
+
+
+if (Number.isNaN(horas) || horas < 0 || minutos > 59){
+   return res.status(400).json({error: "La cantidad de horas ingresadas"}) 
+}
+
+const tarifa=tipo==="carro"? 1200 : 500
+
+let h=Number(horas);
+let m=Number(minutos);
+
+if(m>5) h++;
+
+const total=h*tarifa;
+
+res.json({
+    placa: placa,
+    tipo: tipo,
+    tarifa: tarifa,
+    tiempoEnUso: horas + ":" + minutos,
+    horaCobradas: h,
+    total: total
+});
+
 });
 
 app.listen(PORT, () => {
