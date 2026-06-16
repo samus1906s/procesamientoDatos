750e8ed13f4832037dfc2aaa1e397af0a76f3584
Samus1906s <sacerdasce@est.utn.ac.cr>
2026-06-16 09:50:31 -0600

    Versión 2, trabajamos en la creación de variables de entorno

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,20 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const NAME = process.env.SERVER_NAME;
+const VERSION = process.env.SERVER_VERSION;
+const DESCRIPTION = process.env.SERVER_DESCRIPTION;
+const PORT = process.env.SERVER_PORT || 4000;
 
 const app = express();
 
-app.get( "/", (reg, res) => {
-    res.send( <h1>Servidor funcionando</h1>)
+
+app.get( '/', (reg, res) => {
+    res.send( '<h1>Servidor funcionando</h1>')
 });
 
-app.listen( () => {
-  console.log(`API ejecutándose en 4000 `);
+app.listen(PORT, () => {
+  console.log(`API ejecutándose en http://localhost:${PORT}`);
 });
